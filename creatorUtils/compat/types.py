afe971f7944aa173d5ecb2cca90dac6b1c8178c2
Ken Peterson <arceusthe@gmail.com>
2018-08-09 15:36:35 -0700

    Add files via upload

--- creatorUtils/compat/types.py
+++ creatorUtils/compat/types.py
@@ -7,16 +7,17 @@ scrits.
 
 To check if a variable is a string or unicode
 (or just a string in Python 3), use:
+
 	`type(var) in stringType`
+			or
+	`isinstance(var, stringType)`
 
 to check if a variable is an integer or long (or just
 an integer in Python 3) use:
 
 	`type(var) in intlong`
-
-To make a string into a value that can be succesfully
-written to a binary file every time, use:
-	`binaryString(var)`
+			or
+	`isinstance(var, intlong)`
 """
 
 import sys
@@ -24,28 +25,15 @@ import sys
 if sys.version_info[0] < 3:
 	stringType = [str, unicode];
 	intlong = [int, long];
-	def binString(inp):
-		if isinstance(inp, unicode):
-			try:
-				return inp.encode('latin-1');
-			except:
-				return inp.encode('utf-8');
-		else:
-			return inp.decode('latin-1').encode('latin-1');
+	bytes = bytearray;
 else:
 	stringType = [str];
 	intlong = [int];
-	bytearray = bytes;
 	import io;
 	file = io.IOBase;
-	def binString(inp):
-		if isinstance(inp, bytes):
-			return inp;
-		else:
-			try:
-				return inp.encode('latin-1');
-			except:
-				return inp.encode('utf-8');
 
-function = binString.__class__;
+def a():
+	pass;
+
+function = a.__class__;
 builtin_function = hex.__class__;
