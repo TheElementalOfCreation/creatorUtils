dd9abf0aad909dbc179b0de8f64589ed77796a5a
Ken Peterson <arceusthe@gmail.com>
2019-04-01 18:53:11 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("LICENSE", "r") as fh:
 
 setuptools.setup(
 	name = "creatorUtils",
-	version = "0.2.3",
+	version = "0.2.5",
 	author = "The Elemental of Creation",
 	author_email = "arceusthe@gmail.com",
 	description = "creatorUtils main package",
